4b0a731ca5bf195450f5dfc92ac3064fd7f5957f
VelvetHawk <artem.smnv@yahoo.ie>
2019-11-09 02:07:14 +0100

    Added basic JSON dummy response

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,17 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
-
-
-class Server(BaseHTTPRequestHandler):
+class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        pass
+        payload = json.dumps(
+            {
+                "message": "hi"
+            }
+        )
+        self.send_response(code=200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", len(payload))
+        self.end_headers()
+        # Return response
+        self.wfile.write(bytes(payload, encoding="utf-8"))
+
--- server/__main__.py
+++ server/__main__.py
@@ -1,17 +1,20 @@
-from . import Server
 from http.server import HTTPServer
 import time
+from . import WebServer
 
 
 host = "localhost"
 port = 8081
+webServer = None
 
 try:
     print("Starting Server...")
-    webServer = HTTPServer((host, port), Server())
+    webServer = HTTPServer((host, port), WebServer)
     print("Server listening on http://{}:{}".format(host, port))
+    webServer.serve_forever()
 except KeyboardInterrupt:
     print("Exiting server...")
 finally:
-    webServer.server_close()
-    print("Server stopped")
+    if webServer:
+        webServer.server_close()
+        print("Server stopped")
